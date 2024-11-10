07ca62b0a180fe242c2b8261345398c0d3fa758f
cucumber <coma2ss@gmail.com>
2024-11-10 16:03:38 +0300

    add cmd name

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import sys
 import asyncio
 from src import *
 from src.deeplchain import _banner, _clear
+from platform import system as s_name
+from os import system as sss
 
 if __name__ == "__main__":
+    if s_name() == 'Windows':
+        sss(f'cls && title Otter_loot')
+    else:
+        sss('clear')
     _clear()
     _banner()
     while True:
